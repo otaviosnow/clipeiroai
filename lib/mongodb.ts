5a349be3f51064a76be6810ba24b48f359e0d3fe
otaviosnow <otaviosnow@github.com>
2025-09-25 21:08:53 -0300

    Fix MONGODB_URI TypeScript error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -31,7 +31,7 @@ async function connectDB() {
       bufferCommands: false,
     }
 
-    cached!.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    cached!.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
       console.log('✅ MongoDB connected successfully')
       return mongoose
     })
